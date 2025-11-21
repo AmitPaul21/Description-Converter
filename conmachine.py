b3b82186c3b16a2b657c55218c6a83f814f4c448
AmitPaul21 <46259933+AmitPaul21@users.noreply.github.com>
2025-11-21 09:29:57 -0700

    Update conmachine.py
    update final version 2

--- conmachine.py
+++ conmachine.py
@@ -306,12 +306,12 @@ st.title("Description Converter 📝")
 
 st.markdown("""
 ### Instructions 📌
-Upload your .docx file and enter the well name.  
-This tool will:  
-- Fix formatting  
-- Fix lithology capitalization  
-- Fix percentages  
-- Highlight depth & % errors  
+1. Create a Word document and insert the text file for the Build Section. Go to Layout > Breaks > Page Break. Insert the text file for the Horizontal Section. Save the Word file.
+2. Upload the saved file here.
+3. Also, provide the well name for the file which will appear in the header section.
+4. Download the processed file once ready.
+5. If there are any issues in the depth, it will be marked as yellow.
+6. If there are any issues in the % values, it will be marked as red.
 """)
 
 uploaded_file = st.file_uploader("Upload your Word file (.docx)", type=["docx"])
